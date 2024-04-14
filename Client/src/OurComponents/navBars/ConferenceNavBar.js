2a3cf96167db7f563b7924c336a3bf1c72f24ab6
Bernardo Pires <bernardopires302@gmail.com>
2024-04-14 17:47:56 +0100

    Start of the conferencePage
    Created pages and files (non empty files have been copied from already existing files):
    - BiddingPage (Empty)
    - ComiteManagementPage (Empty)
    - ConferenceDefinitionsPage (Empty)
    - EmailsPage (Empty)
    - ReviewsPage (Empty)
    - SubmissionsPage (Empty)
    - ConferencePage
    - conferenceRoutes (Already edited to what we need)
    - ConferenceLayout (Only Copied)
    - ConferenceNavbarMobile (Only Copied)
    - ConferenceNavbar (Next Step: Use conferencerRoutes to create de menu bar dynamically)

---
+++ Client/src/OurComponents/navBars/ConferenceNavBar.js
@@ -0,0 +1,168 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState, useEffect } from "react";
+
+// react-router components
+import { Link } from "react-router-dom";
+
+// prop-types is a library for typechecking of props.
+import PropTypes from "prop-types";
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
+
+// Material Dashboard 2 React example components
+import DefaultNavbarLink from "examples/Navbars/DefaultNavbar/DefaultNavbarLink";
+import DefaultNavbarMobile from "examples/Navbars/DefaultNavbar/DefaultNavbarMobile";
+
+// Material Dashboard 2 React base styles
+import breakpoints from "assets/theme/base/breakpoints";
+
+// Material Dashboard 2 React context
+import { useMaterialUIController } from "context";
+
+export default function ConferenceNavBar({ transparent, light, action }) {
+  const [controller] = useMaterialUIController();
+  const { darkMode } = controller;
+
+  const [mobileNavbar, setMobileNavbar] = useState(false);
+  const [mobileView, setMobileView] = useState(false);
+
+  const openMobileNavbar = ({ currentTarget }) => setMobileNavbar(currentTarget.parentNode);
+  const closeMobileNavbar = () => setMobileNavbar(false);
+
+  useEffect(() => {
+    // A function that sets the display state for the DefaultNavbarMobile.
+    function displayMobileNavbar() {
+      if (window.innerWidth < breakpoints.values.lg) {
+        setMobileView(true);
+        setMobileNavbar(false);
+      } else {
+        setMobileView(false);
+        setMobileNavbar(false);
+      }
+    }
+
+    /** 
+     The event listener that's calling the displayMobileNavbar function when 
+     resizing the window.
+    */
+    window.addEventListener("resize", displayMobileNavbar);
+
+    // Call the displayMobileNavbar function to set the state with the initial value.
+    displayMobileNavbar();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", displayMobileNavbar);
+  }, []);
+
+  return (
+    <Container>
+      {/* Esta MDBox define a barra onde estarão as opções do menu */}
+      <MDBox
+        px={{ xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2 }}
+        mx={3}
+        width="calc(100% - 48px)"
+        borderRadius="lg"
+        shadow={transparent ? "none" : "md"}
+        color={light ? "white" : "dark"}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        position="absolute"
+        left={0}
+        zIndex={3}
+        sx={({
+          palette: { transparent: transparentColor, white, background },
+          functions: { rgba },
+        }) => ({
+          backgroundColor: transparent
+            ? transparentColor.main
+            : rgba(darkMode ? background.sidenav : white.main, 0.8),
+          backdropFilter: transparent ? "none" : `saturate(200%) blur(30px)`,
+        })}
+      >
+        {/* Esta MDBox contem os links. É onde estão definidas as primeiras opções do menu (Submissões / Bidding / Reviews / Envio de Mails / Gestão do Comitê / Definições da Conferência) */}
+        <MDBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
+          <DefaultNavbarLink icon="dashboard" name="Return to Home" route="/HomePage" light={light} />
+          <DefaultNavbarLink
+            icon="key"
+            name="sign in"
+            route="/Signin"
+            light={light}
+          />
+          <DefaultNavbarLink
+            icon="account_circle"
+            name="sign up"
+            route="/Signup"
+            light={light}
+          />   
+        </MDBox>
+       
+        {/* Esta MDBox define a navbar quando estamos em modo mobile */}          
+        <MDBox
+          display={{ xs: "inline-block", lg: "none" }}
+          lineHeight={0}
+          py={1.5}
+          pl={1.5}
+          color="inherit"
+          sx={{ cursor: "pointer" }}
+          onClick={openMobileNavbar}
+        >
+          <Icon fontSize="default">{mobileNavbar ? "close" : "menu"}</Icon>
+        </MDBox>
+      </MDBox>
+      {mobileView && <DefaultNavbarMobile open={mobileNavbar} close={closeMobileNavbar} />}
+    </Container>
+  );
+}
+
+// Setting default values for the props of DefaultNavbar
+SignInOutNavBar.defaultProps = {
+  transparent: false,
+  light: false,
+  action: false,
+};
+
+// Typechecking props for the SignInOutNavBar
+SignInOutNavBar.propTypes = {
+  transparent: PropTypes.bool,
+  light: PropTypes.bool,
+  action: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.shape({
+      type: PropTypes.oneOf(["external", "internal"]).isRequired,
+      route: PropTypes.string.isRequired,
+      color: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "info",
+        "success",
+        "warning",
+        "error",
+        "dark",
+        "light",
+      ]),
+      label: PropTypes.string.isRequired,
+    }),
+  ]),
+};
---
+++ Client/src/conferenceRoutes.js
@@ -0,0 +1,78 @@
+import PageNotFound from "pages/PageNotFound";
+import Submissions from "pages/ConferencePages/SubmissionsPage";
+import Bidding from "pages/ConferencePages/BiddingPage";
+import Reviews from "pages/ConferencePages/ReviewsPage";
+import Mails from "pages/ConferencePages/EmailsPage";
+import ComiteManagement from "pages/ConferencePages/ComiteManagement";
+import ConferenceDefinitions from "pages/ConferencePages/ConferenceDefinitionsPage";
+
+// @mui icons
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Submissions",
+    key: "Submissions",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/Submissions",
+    component: <Submissions />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "Bidding",
+    key: "Bidding",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/Bidding",
+    component: <Bidding />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "Reviews",
+    key: "Reviews",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/Reviews",
+    component: <Reviews />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "Envio de Mails",
+    key: "Envio de Mails",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/EMails",
+    component: <Mails />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "Gestão do Comitê",
+    key: "Gestão do Comitê",
+    icon: <Icon fontSize="small">login</Icon>,
+    route: "/ComiteManagement",
+    component: <ComiteManagement />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "Definições da Conferência",
+    key: "Definições da Conferência",
+    icon: <Icon fontSize="small">login</Icon>,
+    route: "/ConferenceDefinitions",
+    component: <ConferenceDefinitions />,
+    display: true,
+  },
+  {
+    type: "collapse",
+    name: "404",
+    key: "404",
+    icon: <Icon fontSize="small">assignment</Icon>,
+    route: "/PageNotFound",
+    component: <PageNotFound />,
+    display: false,
+  },
+];
+
+export default routes;
