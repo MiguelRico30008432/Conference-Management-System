f6b7a8fa4b6e1a07bb10a8eddec8d578f59e2a0a
Miguel Rico <miguelrr314@gmail.com>
2024-04-14 22:49:36 +0100

    my Profile Page - wip

--- Server/routes/database.js
+++ Server/routes/database.js
@@ -21,10 +21,16 @@ router.post("/acceptOrRejectConference", async function (req, res) {
   try {
     //Update Info on Database
     await db.updateData(
-      "conferences",{ confapproved: req.body.acceptOrReject },{ confid: req.body.confid }
+      "conferences",
+      { confapproved: req.body.acceptOrReject },
+      { confid: req.body.confid }
     );
     // Fetch the Email from Database
-    const userRecords = await db.fetchData("users", "userid", req.body.confowner);
+    const userRecords = await db.fetchData(
+      "users",
+      "userid",
+      req.body.confowner
+    );
     if (userRecords.length === 0) {
       return res.status(404).send({ msg: "User not found" });
     }
@@ -33,14 +39,27 @@ router.post("/acceptOrRejectConference", async function (req, res) {
     console.log(userEmail);
 
     // Send Email
-    const subject = req.body.acceptOrReject === 2 ? "Your conference as been Accepted" : "Your conference as been Rejected";
-    const contentsubject = req.body.acceptOrReject === 2 ? "Accepted" : "Rejected";
+    const subject =
+      req.body.acceptOrReject === 2
+        ? "Your conference as been Accepted"
+        : "Your conference as been Rejected";
+    const contentsubject =
+      req.body.acceptOrReject === 2 ? "Accepted" : "Rejected";
     const content = `Your conference submission has been ${contentsubject.toLowerCase()}. Thank you for your patience.`;
     sendEmail(userEmail, subject, content);
 
     return res.status(200).send({ msg: "Update successful, email sent." });
   } catch (error) {
     console.error("Error in /acceptOrRejectConference:", error);
+    return res.status(500).send({ msg: "Internal Error" });
+  }
+});
+
+router.post("/userData", auth.ensureAuthenticated, async (req, res) => {
+  try {
+    const result = await db.fetchUserData("userid", req.body.userID);
+    return res.status(200).send(result);
+  } catch (error) {
     return res.status(500).send({ msg: "Internal Error" });
   }
 });
