88d5a21769da4945e90caddc5400aa85a3850eb9
Miguel Rico <miguelrr314@gmail.com>
2024-04-14 12:20:39 +0100

    fix bug -> auth data disappear in every refresh

--- Client/src/auth.context.js
+++ Client/src/auth.context.js
@@ -4,10 +4,21 @@ import PropTypes from "prop-types";
 const AuthContext = React.createContext();
 
 function AuthProviderWrapper(props) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [isAdmin, setIsAdmin] = useState(null);
-  const [userRole, setUserRole] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+  const [user, setUser] = useState(localStorage.getItem("user") === "true");
+  const [isAdmin, setIsAdmin] = useState(
+    localStorage.getItem("isAdmin") === "true"
+  );
+  const [userRole, setUserRole] = useState(localStorage.getItem("userRole"));
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+    localStorage.setItem("user", user);
+    localStorage.setItem("isAdmin", isAdmin);
+    localStorage.setItem("userRole", userRole);
+  }, [isLoggedIn, user, isAdmin, userRole]);
 
   return (
     <AuthContext.Provider
