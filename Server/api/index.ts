1ea94e7388ad852b9db0fe6adcdfe6d8894ec8d6
Xurumbambo <30009280@students.ual.pt>
2024-05-16 23:03:13 +0100

    debug6

--- Server/api/index.ts
+++ Server/api/index.ts
@@ -69,6 +69,15 @@ app.use(routes);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+// Additional Preflight Request Handling
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204); // No Content
+});
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
 module.exports = app;
