7794b36d7cf26426a2214af0f0fd36749ed19833
Xurumbambo <30009280@students.ual.pt>
2024-05-21 18:01:06 +0100

    AllSubmissions Done

--- Client/src/pages/ConferencePages/AllSubmissions.js
+++ Client/src/pages/ConferencePages/AllSubmissions.js
@@ -11,19 +11,21 @@ import MDTypography from "components/MDTypography";
 import MDButton from "components/MDButton";
 import LoadingCircle from "OurComponents/loading/LoadingCircle";
 import Alert from "@mui/material/Alert";
-import { v4 as uuidv4 } from "uuid";
 import CompleteTable from "OurComponents/Table/CompleteTable";
-import SubmissionsDetails from "OurComponents/Info/SubmissionDetails";
+import SubmissionDetails from "OurComponents/Info/SubmissionDetails";
 import Footer from "OurComponents/footer/Footer";
+import PopUpWithMessage from "OurComponents/Info/PopUpWithMessage"; // Ensure this import is correct
 
 export default function AllSubmissions() {
-  const { confID } = useContext(ConferenceContext);
+  const { confID, userRole } = useContext(ConferenceContext);
   const { isLoggedIn } = useContext(AuthContext);
   const [openLoading, setOpenLoading] = useState(false);
   const [message, setMessage] = useState(null);
   const [rows, setRows] = useState([]);
   const [detailsOpen, setDetailsOpen] = useState(false);
   const [dataForDetails, setDataForDetails] = useState({});
+  const [openPopUpMessage, setOpenPopUpMessage] = useState(false);
+  const [dataForDelete, setDataForDelete] = useState(null);
 
   useEffect(() => {
     async function fetchAllSubmissions() {
@@ -45,10 +47,16 @@ export default function AllSubmissions() {
         const jsonResponse = await response.json();
 
         if (response.status === 200) {
-          for (let line of jsonResponse) {
-            line.id = uuidv4();
-            setRows((allExistingRows) => [...allExistingRows, line]);
-          }
+          const transformedData = jsonResponse.map((submission) => ({
+            id: submission.id,
+            title: submission.title,
+            authors: submission.authors,
+            status: submission.status ? "Accepted" : "Pending",
+            addDate: submission.adddate,
+            abstract: submission.abstract,
+            fileUrl: submission.fileUrl,
+          }));
+          setRows(transformedData);
         } else {
           setMessage(<Alert severity="error">{jsonResponse.message}</Alert>);
         }
@@ -65,18 +73,131 @@ export default function AllSubmissions() {
     }
   }, [confID, isLoggedIn]);
 
+  const handleDelete = async () => {
+    setMessage(null);
+    setOpenLoading(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/deleteSubmission`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            submissionID: dataForDelete,
+          }),
+        }
+      );
+
+      const jsonResponse = await response.json();
+
+      if (response.ok) {
+        setRows((rows) => rows.filter((row) => row.id !== dataForDelete));
+        setMessage(
+          <Alert severity="success">Submission deleted successfully</Alert>
+        );
+      } else {
+        setMessage(<Alert severity="error">{jsonResponse.msg}</Alert>);
+      }
+    } catch (error) {
+      setMessage(<Alert severity="error">Could not delete submission</Alert>);
+    }
+
+    setOpenLoading(false);
+  };
+
+  const handleDownload = async (submission) => {
+    setMessage(null);
+    setOpenLoading(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/downloadSubmissionFile`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            submissionID: submission.id,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        const jsonResponse = await response.json();
+        setMessage("Failed to download file: " + jsonResponse.msg);
+        return;
+      }
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = submission.title + ".pdf"; // Set the file name
+      a.click();
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    } catch (error) {
+      setMessage(<Alert severity="error">Could not download file</Alert>);
+    }
+
+    setOpenLoading(false);
+  };
+
   const columns = [
     { field: "title", headerName: "Title", width: 200 },
     { field: "status", headerName: "Status", width: 120 },
     { field: "authors", headerName: "Authors", width: 200 },
-    { field: "adddate", headerName: "Submission Date", width: 120 },
+    { field: "addDate", headerName: "Submission Date", width: 120 },
+    {
+      field: "download",
+      filterable: false,
+      headerName: "",
+      description: "",
+      sortable: false,
+      disableColumnMenu: true,
+      resizable: false,
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <MDButton
+              variant="gradient"
+              color="warning"
+              onClick={() => handleDownload(params.row)}
+              sx={{
+                maxWidth: "120px",
+                maxHeight: "23px",
+                minWidth: "30px",
+                minHeight: "23px",
+              }}
+            >
+              Download File
+            </MDButton>
+          </div>
+        );
+      },
+    },
     {
       field: "moreInfo",
       headerName: "",
       sortable: false,
       filterable: false,
       disableColumnMenu: true,
-      width: 150,
+      resizable: false,
+      width: 80,
       renderCell: (params) => {
         return (
           <div
@@ -95,7 +216,7 @@ export default function AllSubmissions() {
                 setDetailsOpen(true);
               }}
               sx={{
-                maxWidth: "80px",
+                maxWidth: "60px",
                 maxHeight: "23px",
                 minWidth: "30px",
                 minHeight: "23px",
@@ -107,11 +228,61 @@ export default function AllSubmissions() {
         );
       },
     },
+    ...(userRole === "Chair" || userRole === "Owner"  ? [{
+      field: "delete",
+      filterable: false,
+      headerName: "",
+      description: "",
+      sortable: false,
+      disableColumnMenu: true,
+      resizable: false,
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <MDButton
+              variant="gradient"
+              color="error"
+              onClick={() => {
+                setDataForDelete(params.row.id);
+                setOpenPopUpMessage(true);
+              }}
+              sx={{
+                maxWidth: "130px",
+                maxHeight: "23px",
+                minWidth: "30px",
+                minHeight: "23px",
+              }}
+            >
+              Delete Submission
+            </MDButton>
+          </div>
+        );
+      },
+    }] : [])
   ];
 
   return (
     <>
       {openLoading && <LoadingCircle />}
+      <PopUpWithMessage
+        open={openPopUpMessage}
+        handleClose={() => setOpenPopUpMessage(false)}
+        handleConfirm={async () => {
+          await handleDelete();
+          setOpenPopUpMessage(false);
+        }}
+        title={"Confirm your submission removal"}
+        text={"Are you sure you want to remove this submission?"}
+      />
+
       <DashboardLayout>
         <ConfNavbar />
 
@@ -150,7 +321,7 @@ export default function AllSubmissions() {
         </Container>
 
         {detailsOpen && (
-          <SubmissionsDetails
+          <SubmissionDetails
             submission={dataForDetails}
             onClose={() => setDetailsOpen(false)}
           />
